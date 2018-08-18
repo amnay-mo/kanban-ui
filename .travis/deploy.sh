87bc2175abf93025130bdb6e64b0f7dde75e4113
Amnay <amnay.m@gmail.com>
2018-08-18 10:16:12 +0200

    add ci/cd

---
+++ .travis/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+   curl https://sdk.cloud.google.com | bash;
+fi
+
+openssl aes-256-cbc -K $encrypted_f10587d97a52_key -iv $encrypted_f10587d97a52_iv -in .travis/myfoobarproject-9a043adac874.json.enc -out myfoobarproject-9a043adac874.json -d
+
+gcloud auth activate-service-account --key-file=myfoobarproject-9a043adac874.json
+
+gcloud auth configure-docker
+
+docker build -t eu.gcr.io/myfoobarproject/kanban-ui:${TRAVIS_TAG} -t eu.gcr.io/myfoobarproject/kanban-ui:latest .
+
+docker push eu.gcr.io/myfoobarproject/kanban-ui:${TRAVIS_TAG}
+
+docker push eu.gcr.io/myfoobarproject/kanban-ui:latest
